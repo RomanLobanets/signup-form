d642e024047f241124d88c280199c7fc395499dc
RomanLobanets <lobanets@gmail.com>
2020-08-24 19:33:54 -0400

    Updates

---
+++ precache-manifest.368e8c2cc5a1538debb9bc51df172a5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fca806909f064e959229d8e4501b76ce",
+    "url": "/signup-form/index.html"
+  },
+  {
+    "revision": "6e23b39745c81e0937f8",
+    "url": "/signup-form/static/css/main.59261a03.chunk.css"
+  },
+  {
+    "revision": "6eda2a0384120cd74057",
+    "url": "/signup-form/static/js/2.be6d4749.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/signup-form/static/js/2.be6d4749.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e23b39745c81e0937f8",
+    "url": "/signup-form/static/js/main.c57b9a45.chunk.js"
+  },
+  {
+    "revision": "5fcc0d9a851817b16cff",
+    "url": "/signup-form/static/js/runtime-main.1c2e0be3.js"
+  },
+  {
+    "revision": "412f32896c37205f333f5c7fa9b77585",
+    "url": "/signup-form/static/media/angle-arrow-down.412f3289.svg"
+  }
+]);
